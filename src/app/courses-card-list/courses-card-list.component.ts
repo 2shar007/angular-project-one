d2dd65e1688c7416bbd9344536a969c6008c29f7
2shar007 <2shar00ghodke@gmail.com>
2022-03-30 10:51:50 +0530

    changes made for dialog box

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {Course} from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { filter } from 'rxjs/operators';
+import { openEditCourseDialog } from '../course-dialog/course-dialog.component';
 
 @Component({
     selector: 'courses-card-list',
@@ -12,8 +15,9 @@ export class CoursesCardListComponent implements OnInit {
     @Input()
     courses: Course[];
 
-    constructor() {
-    }
+    constructor(private dialog: MatDialog,
+        private responsive: BreakpointObserver) {
+}
 
     ngOnInit() {
 
@@ -21,7 +25,13 @@ export class CoursesCardListComponent implements OnInit {
 
     editCourse(course:Course) {
 
-
+        openEditCourseDialog(this.dialog, course)
+        .pipe(
+            filter(val => !!val)
+        )
+        .subscribe(
+            val => console.log('new course value:', val)
+        );
     }
 
 }
